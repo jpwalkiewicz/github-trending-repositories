0c35aaf53707613aefbef55efa1991b79abb45a3
jpwalkiewicz <jp.walkiewicz@gmail.com>
2019-02-16 15:36:45 +0100

    Updates

---
+++ precache-manifest.389e7ab81bbe21725e20c3b728c2f38b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7cf9704e86c0d4a8da9",
+    "url": "/github-trending-repositories/static/js/runtime~main.c7cf9704.js"
+  },
+  {
+    "revision": "c3b33f9310d77e3c610c",
+    "url": "/github-trending-repositories/static/js/main.c3b33f93.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/github-trending-repositories/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "c3b33f9310d77e3c610c",
+    "url": "/github-trending-repositories/static/css/main.2be250fc.chunk.css"
+  },
+  {
+    "revision": "4edd3d99e8d5bfddb6a83d4c3b88624c",
+    "url": "/github-trending-repositories/index.html"
+  }
+];
